683758f24e3c77e8e29ebad04a56d0fe535d386d
cosimo <cosimovecchio2006@gmail.com>
2025-10-29 21:19:15 +0100

    Spostati file nella root

---
+++ index.js
@@ -0,0 +1,3 @@
+const CLIENT_ID = '1433147626139947159'; // Il tuo ID applicazione
+const CLIENT_SECRET = 'tlG-7uEpVPqgsQxjMTqkF4Z1QqDJeAbs'; // La tua chiave segreta
+const REDIRECT_URI = 'http://localhost:3000/discord-callback'; // URL PROVVISORIO
