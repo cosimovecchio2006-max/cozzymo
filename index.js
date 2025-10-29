a5895444e90439b8bd2afc83c3c330cd43bf3671
cosimo <cosimovecchio2006@gmail.com>
2025-10-29 22:34:00 +0100

    Corretto package.json finale con dependencies e build script

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const CLIENT_ID = '1433147626139947159'; // Il tuo ID applicazione
 const CLIENT_SECRET = 'tlG-7uEpVPqgsQxjMTqkF4Z1QqDJeAbs'; // La tua chiave segreta
-const REDIRECT_URI = ' https://cosimovecchio2006-max-project.onrender.com/discord-callback';
+const REDIRECT_URI = 'https://cosimovecchio2006-max-project.onrender.com/discord-callback';
